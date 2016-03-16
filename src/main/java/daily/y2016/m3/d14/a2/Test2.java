50979d15149a0c8b0bc420b16f486701d7f239d6
mauersu <740344998@qq.com>
2016-03-16 21:29:52 +0800

    20160316

--- src/main/java/daily/y2016/m3/d14/a2/Test2.java
+++ src/main/java/daily/y2016/m3/d14/a2/Test2.java
@@ -1,11 +1,15 @@
 package daily.y2016.m3.d14.a2;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class Test1 {
+public class Test2 {
 	
 	static AtomicInteger index = new AtomicInteger(1);
 	static Object obj = new Object();
+	static ReentrantLock lock = new ReentrantLock();
+	static Condition condition = lock.newCondition();
 	
 	public static void main(String args[]) throws InterruptedException {
 		
@@ -17,25 +21,24 @@ public class Test1 {
 		t2.start();
 		t3.start();
 		Thread.sleep(500);
-		synchronized(obj) {
 			//obj.notifyAll();
-			obj.notify();
-		}
-		
+		lock.lock();
+		condition.signal();
+		lock.unlock();
 		
 	}
 	
 	public static class TRunnable implements Runnable {
 		
 		public void run() {
-			synchronized(obj) {
-				try {
-					obj.wait();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+			lock.lock();
+			try {
+				condition.await();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+			lock.unlock();
 			System.out.println(index.incrementAndGet());
 		}
 	}
